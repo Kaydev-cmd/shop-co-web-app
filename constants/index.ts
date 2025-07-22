7780482cf2cf4a20df713b6836ae600e9e5a427c
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-22 18:23:58 +0200

    feat: define NAV_LINKS array for Header navigation items

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const NAV_LINKS = [
+  {
+    id: 1,
+    link: "Shop",
+    href: "#",
+  },
+  {
+    id: 2,
+    link: "On Sale",
+    href: "#",
+  },
+  {
+    id: 3,
+    link: "New Arrivals",
+    href: "#",
+  },
+  {
+    id: 4,
+    link: "Brands",
+    href: "#",
+  },
+];

5efd606f28a457e3f2d54e7b429558ad6b93abd1
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-22 17:38:34 +0200

    feat: integrate Banner component into layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { LayoutProps } from "@/interfaces";
+import Banner from "../common/Banner";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
+      <Banner />
       <Header />
       <main className="min-h-screen">{children}</main>
       <Footer />

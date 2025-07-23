e3c681c6d57d6bcb2abcbe20f97cb7c96cd3c441
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 13:49:33 +0200

    feat: create reusable Button component with title and onClick handler

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { ButtonProps } from "@/interfaces";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
+  return (
+
+    <button onClick={onClick} className="bg-black">
+      {title}
+    </button>
+  );
 };
 
 export default Button;

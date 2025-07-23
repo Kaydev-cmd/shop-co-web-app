df4362358e6c39ba443534579952dee760e3e2bf
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 14:00:01 +0200

    feat: define ButtonProps and HeroStatsProps interfaces for component typing

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,3 +3,13 @@ import { ReactNode } from "react";
 export interface LayoutProps {
   children: ReactNode;
 }
+
+export interface ButtonProps {
+  title: string;
+  onClick?: () => void;
+}
+
+export interface HeroStatsProps {
+  amountOfUsers: string;
+  description: string;
+}

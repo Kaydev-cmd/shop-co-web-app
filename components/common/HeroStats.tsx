5353e88f166ac88da8fc8928d2b7e65af5839264
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 13:57:49 +0200

    feat: implement HeroStats component with dynamic user stats and styled layout

---
+++ components/common/HeroStats.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { HERO_STATS } from "@/constants";
+
+const HeroStats = () => {
+  return (
+    <div className="flex gap-2">
+      {HERO_STATS.map((stat) => (
+        <div className="border-r-2 last:border-r-0" style={{marginLeft: "8px"}}>
+          <h3 className="font-extrabold text-xl">{stat.amountOfUsers}</h3>
+          <p>{stat.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HeroStats;

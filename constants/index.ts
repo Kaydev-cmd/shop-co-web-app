2541f7f54071599069a0775d19565b99c47f1ace
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 13:58:56 +0200

    feat: add HERO_LOGOS and HERO_STATS constants for use in Hero section

--- constants/index.ts
+++ constants/index.ts
@@ -20,3 +20,41 @@ export const NAV_LINKS = [
     href: "#",
   },
 ];
+
+export const HERO_LOGOS = [
+  {
+    logo: "/assets/images/logos/Logo_1.png",
+    href: "Versace",
+  },
+  {
+    logo: "/assets/images/logos/zara-logo.png",
+    href: "Zara",
+  },
+  {
+    logo: "/assets/images/logos/gucci-logo.png",
+    href: "Gucci",
+  },
+  {
+    logo: "/assets/images/logos/prada-logo.png",
+    href: "Prada",
+  },
+  {
+    logo: "/assets/images/logos/calvin-klein-logo.png",
+    href: "Calvin-Klein",
+  },
+];
+
+export const HERO_STATS = [
+  {
+    amountOfUsers: "200+",
+    description: "International Brands",
+  },
+  {
+    amountOfUsers: "2,000+",
+    description: "High-Quality Products",
+  },
+  {
+    amountOfUsers: "30,000+",
+    description: "Happy Customers",
+  },
+];

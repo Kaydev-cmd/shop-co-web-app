2e9a3c462c70e552da37bfc36f2d59a5bf877e09
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 18:05:20 +0200

    feat: implement ReviewCard component displaying user reviews with rating and verified badge

---
+++ components/common/ReviewCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReviewCardProps } from "@/interfaces";
+import { FaStar } from "react-icons/fa";
+import { MdVerifiedUser } from "react-icons/md";
+
+const ReviewCard: React.FC<ReviewCardProps> = ({ nameOfUser, review }) => {
+  return (
+    <div className="card border border-gray-300 rounded-2xl flex flex-col gap-3">
+      <div className="flex items-center gap-2">
+        <FaStar color="#FFC633" size={20} />
+        <FaStar color="#FFC633" size={20} />
+        <FaStar color="#FFC633" size={20} />
+        <FaStar color="#FFC633" size={20} />
+        <FaStar color="#FFC633" size={20} />
+      </div>
+      <div className="flex items-center gap-1">
+        <h4 className="font-bold text-xl">{nameOfUser}</h4>
+        <MdVerifiedUser color="#01AB31" />
+      </div>
+      <p>{review}</p>
+    </div>
+  );
+};
+
+export default ReviewCard;

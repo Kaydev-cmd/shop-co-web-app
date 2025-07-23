a9f562b58658efcb7f751a7e8d8cdc60cfcc67df
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 16:46:37 +0200

    feat: add unique key to HeroStats items for optimized rendering

--- components/common/HeroStats.tsx
+++ components/common/HeroStats.tsx
@@ -5,7 +5,11 @@ const HeroStats = () => {
   return (
     <div className="flex gap-2">
       {HERO_STATS.map((stat) => (
-        <div className="border-r-2 last:border-r-0" style={{marginLeft: "8px"}}>
+        <div
+          className="border-r-2 last:border-r-0"
+          style={{ marginLeft: "8px" }}
+          key={stat.description}
+        >
           <h3 className="font-extrabold text-xl">{stat.amountOfUsers}</h3>
           <p>{stat.description}</p>
         </div>

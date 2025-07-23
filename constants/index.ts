f2f88c7aecec5d10c4df44a5cc694774a202c9b8
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 18:06:19 +0200

    feat: add REVIEWS mock data array with user testimonials for ReviewCard component

--- constants/index.ts
+++ constants/index.ts
@@ -112,3 +112,21 @@ export const TOP_SELLING = [
     price: "$210",
   },
 ];
+
+export const REVIEWS = [
+  {
+    nameOfUser: "Sarah M.",
+    review:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+  },
+  {
+    nameOfUser: "Alex K.",
+    review:
+      "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.",
+  },
+  {
+    nameOfUser: "James L.",
+    review:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+  },
+];

4f6a37c1a4026d8626db35e0036108935e910b13
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-23 17:15:12 +0200

    feat: add Top Selling section with product grid and View All button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "@/components/common/Hero";
 import ProductCard from "@/components/common/ProductCard";
-import { PRODUCTS } from "@/constants";
+import { NEW_ARRIVALS, TOP_SELLING } from "@/constants";
 import Button from "@/components/common/Button";
 
 const Home = () => {
@@ -10,16 +10,29 @@ const Home = () => {
       <Hero />
 
       {/* New Arrivals */}
-      <section id="newArrivals">
+      <section id="newArrivals" style={{ padding: "" }}>
         <div className="container flex flex-col gap-4">
-          <h3
-            className="font-extrabold text-4xl text-center"
-            style={{ marginBottom: "16px" }}
-          >
-            New Arrivals
-          </h3>
+          <h3 className="font-extrabold text-4xl text-center">New Arrivals</h3>
           <div className="grid grid-cols-1">
-            {PRODUCTS.map((product) => (
+            {NEW_ARRIVALS.map((product) => (
+              <ProductCard
+                image={product.image}
+                alt={product.alt}
+                itemName={product.itemName}
+                price={product.price}
+              />
+            ))}
+          </div>
+          <Button title="View All" />
+        </div>
+      </section>
+
+      {/* Top Selling */}
+      <section id="topSelling">
+        <div className="container flex flex-col gap-4">
+          <h3 className="font-extrabold text-4xl text-center">Top Selling</h3>
+          <div className="grid grid-cols-1">
+            {TOP_SELLING.map((product) => (
               <ProductCard
                 image={product.image}
                 alt={product.alt}

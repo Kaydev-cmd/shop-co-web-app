0a33221b2a9504800b0630e7f5ea0b125ac771e6
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-28 18:24:45 +0200

    feat: add 'You Might Also Like' section with Swiper carousel and subscribe component below ProductDetails

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,8 +1,53 @@
 import React from "react";
 import ProductDetails from "@/components/products/ProductDetails";
+import { TOP_SELLING } from "@/constants";
+import ProductCard from "@/components/common/ProductCard";
+import Subscribe from "@/components/common/Subscribe";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const ProductPage = () => {
-  return <ProductDetails />;
+  return (
+    <>
+      <ProductDetails />
+
+      {/* You might also like */}
+      <section id="alsoLike">
+        <div className="container flex flex-col gap-4">
+          <h3 className="font-extrabold text-4xl text-center uppercase">
+            You Might Also Like
+          </h3>
+          <div>
+            <Swiper pagination={true} modules={[Pagination]}>
+              {TOP_SELLING.map((product) => (
+                <SwiperSlide>
+                  <ProductCard
+                    key={product.itemName}
+                    image={product.image}
+                    alt={product.alt}
+                    itemName={product.itemName}
+                    price={product.price}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <Subscribe />
+      </section>
+    </>
+  );
 };
 
 export default ProductPage;

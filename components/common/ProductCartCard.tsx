1f4df130cad3e4453ed24c1ce1c12dc7bcfa583f
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-29 17:07:04 +0200

    feat: create ProductCartCard component with product info, image, trash icon, and quantity counter

---
+++ components/common/ProductCartCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ProductCartCardProps } from "@/interfaces";
+import { FaTrashCan } from "react-icons/fa6";
+import Counter from "./Counter";
+
+const ProductCartCard: React.FC<ProductCartCardProps> = ({
+  productImage,
+  productName,
+  productSize,
+  productColor,
+  productPrice,
+}) => {
+  return (
+    <div
+      className="flex justify-around gap-4 items-center"
+      style={{ padding: "12px 0" }}
+    >
+      <div>
+        <img src={productImage} alt={productName} className="rounded-xl" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between">
+          <h3 className="text-lg font-bold">{productName}</h3>
+          <FaTrashCan size={20} color="#FF3333" />
+        </div>
+        <p className="text-sm">
+          Size: <span className="text-[#666666]">{productSize}</span>{" "}
+        </p>
+        <p className="text-sm">
+          Color: <span className="text-[#666666]">{productColor}</span>{" "}
+        </p>
+        <div className="flex items-center gap-12">
+          <p className="font-bold text-lg">{productPrice}</p>
+          <Counter />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCartCard;

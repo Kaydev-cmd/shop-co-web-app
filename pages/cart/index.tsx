92db38027d4db5298a4d9e230637189eae4d1e36
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-29 17:10:43 +0200

    feat: implement CartPagewith product listing, order summary, and subscribe section

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import ProductCartCard from "@/components/common/ProductCartCard";
+import { FaArrowRight } from "react-icons/fa6";
+import { CART_ITEMS } from "@/constants";
+import Subscribe from "@/components/common/Subscribe";
 
 const CartPage = () => {
-  return <div>CartPage</div>;
+  return (
+    <div className="container">
+      <h1 className="font-extrabold text-2xl" style={{ marginBottom: "20px" }}>
+        Your Cart
+      </h1>
+      <div className="flex flex-col gap-6">
+        <div className="card border border-gray-300 flex flex-col gap-4 rounded-xl">
+          {CART_ITEMS.map((item) => (
+            <div key={item.id}>
+              <ProductCartCard
+                productImage={item.productImage}
+                productName={item.productName}
+                productSize={item.productSize}
+                productColor={item.productColor}
+                productPrice={item.productPrice}
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Order Summary */}
+        <div className="flex flex-col card border border-gray-300 gap-3 rounded-xl">
+          <h2 className="font-bold text-xl">Order Summary</h2>
+          <div className="flex justify-between">
+            <p className="text-[#666666]">Subtotal</p>
+            <p className="font-bold">$565</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-[#666666]">Delivery Fee</p>
+            <p className="font-bold">$15</p>
+          </div>
+
+          <div className="container">
+            <div className="border border-gray-300"></div>
+          </div>
+
+          <div className="flex justify-between">
+            <p className="text-[#666666]">Total</p>
+            <p className="font-bold">$467</p>
+          </div>
+
+          <div className="flex justify-center">
+            <button className="bg-black flex items-center gap-4">
+              Go to Checkout <FaArrowRight size={24} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <section>
+        <Subscribe />
+      </section>
+    </div>
+  );
 };
 
 export default CartPage;

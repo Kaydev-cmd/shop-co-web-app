c76baf7d31be0a87d7a7d51eba265c519b17efda
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-08-01 17:29:51 +0200

    feat: display rating in user cards using Rating component

--- components/common/ReviewCard.tsx
+++ components/common/ReviewCard.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import { ReviewCardProps } from "@/interfaces";
 import { FaStar } from "react-icons/fa";
 import { MdVerifiedUser } from "react-icons/md";
+import Rating from "./Rating";
 
 const ReviewCard: React.FC<ReviewCardProps> = ({ nameOfUser, review }) => {
   return (
     <div className="card border border-gray-300 rounded-2xl flex flex-col gap-3">
-      <div className="flex items-center gap-2">
-        <FaStar color="#FFC633" size={20} />
-        <FaStar color="#FFC633" size={20} />
-        <FaStar color="#FFC633" size={20} />
-        <FaStar color="#FFC633" size={20} />
-        <FaStar color="#FFC633" size={20} />
-      </div>
+      <Rating />
       <div className="flex items-center gap-1">
         <h4 className="font-bold text-xl">{nameOfUser}</h4>
         <MdVerifiedUser color="#01AB31" />

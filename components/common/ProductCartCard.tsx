d50e0c4c5fdd3ce9474e11c7caea5f2eb53bd4aa
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 13:45:50 +0200

    feat: apply text-xl for the productName and text-md for productSize, productColor, and productPrice for medium screens

--- components/common/ProductCartCard.tsx
+++ components/common/ProductCartCard.tsx
@@ -20,17 +20,17 @@ const ProductCartCard: React.FC<ProductCartCardProps> = ({
       </div>
       <div className="flex flex-col gap-2">
         <div className="flex items-center justify-between">
-          <h3 className="text-lg font-bold">{productName}</h3>
+          <h3 className="text-lg font-bold md:text-xl">{productName}</h3>
           <FaTrashCan size={20} color="#FF3333" />
         </div>
-        <p className="text-sm">
+        <p className="text-sm md:text-md">
           Size: <span className="text-[#666666]">{productSize}</span>{" "}
         </p>
-        <p className="text-sm">
+        <p className="text-sm md:text-md">
           Color: <span className="text-[#666666]">{productColor}</span>{" "}
         </p>
         <div className="flex items-center gap-12">
-          <p className="font-bold text-lg">{productPrice}</p>
+          <p className="font-bold text-lg md:text-xl">{productPrice}</p>
           <Counter />
         </div>
       </div>

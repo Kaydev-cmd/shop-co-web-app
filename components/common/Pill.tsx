3c2609c29e3072216f668e00cac475a16fbc7618
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 13:44:25 +0200

    feat: add a width of 140px, apply text center, and large text for medium screens

--- components/common/Pill.tsx
+++ components/common/Pill.tsx
@@ -3,8 +3,8 @@ import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className="bg-gray-200 rounded-4xl hover:bg-black hover:text-white" style={{ padding: "12px" }}>
-      <p>{title}</p>
+    <div className="bg-gray-200 rounded-4xl hover:bg-black hover:text-white md:w-[140px]" style={{ padding: "12px" }}>
+      <p className="text-center md:text-lg">{title}</p>
     </div>
   );
 };

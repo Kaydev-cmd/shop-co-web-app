a95ba5ac7bfd501ac609cf89a96c8fda1430a931
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 13:52:05 +0200

    feat: add Subscribe section inside container layout

--- pages/top-selling/index.tsx
+++ pages/top-selling/index.tsx
@@ -3,29 +3,36 @@ import { TOP_SELLING } from "@/constants";
 import Link from "next/link";
 import ProductCard from "@/components/common/ProductCard";
 import Button from "@/components/common/Button";
+import Subscribe from "@/components/common/Subscribe";
 
 const TopSellingPage = () => {
   return (
-    <section>
-      <div className="container flex flex-col gap-4">
-        <h3 className="font-extrabold text-4xl text-center">Top Selling</h3>
-        <div>
-          {TOP_SELLING.map((product) => (
-            <Link href={`/product/${product.id}`} key={product.id}>
-              <ProductCard
-                key={product.itemName}
-                image={product.image}
-                alt={product.alt}
-                itemName={product.itemName}
-                rating={product.rating}
-                price={product.price}
-              />
-            </Link>
-          ))}
+    <>
+      <section>
+        <div className="container flex flex-col gap-4">
+          <h3 className="font-extrabold text-4xl text-center">Top Selling</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {TOP_SELLING.map((product) => (
+              <Link href={`/product/${product.id}`} key={product.id}>
+                <ProductCard
+                  key={product.itemName}
+                  image={product.image}
+                  alt={product.alt}
+                  itemName={product.itemName}
+                  rating={product.rating}
+                  price={product.price}
+                />
+              </Link>
+            ))}
+          </div>
+          <Button title="View More" />
         </div>
-        <Button title="View More" />
-      </div>
-    </section>
+      </section>
+
+      <section className="container">
+        <Subscribe />
+      </section>
+    </>
   );
 };
 

a6d7eaf043f9691690ac359be2a1d6926652f83c
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 13:41:18 +0200

    feat: add a width of 160px and a medium text size for the Counter component

--- components/common/Counter.tsx
+++ components/common/Counter.tsx
@@ -7,8 +7,8 @@ const Counter = () => {
 
   return (
     <div
-      className="bg-gray-200 rounded-4xl flex items-center gap-4"
-      style={{ padding: "8px 16px" }}
+      className="bg-gray-200 rounded-4xl flex items-center justify-center gap-6 md:w-[160px] md:text-md"
+      style={{ padding: "12px 16px" }}
     >
       <FaMinus onClick={() => setCount((prev) => (prev > 0 ? prev - 1 : 0))} />
       <p>{count}</p>

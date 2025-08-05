05d7a983eea844ecd50f51f9f31c4fe0f393956e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 14:49:04 +0200

    feat: add conditional styling and click handler to Pill component for single selection

--- components/common/Pill.tsx
+++ components/common/Pill.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { PillProps } from "@/interfaces";
 
-const Pill: React.FC<PillProps> = ({ title }) => {
+const Pill: React.FC<PillProps> = ({ title, selected, onClick }) => {
   return (
-    <div className="bg-gray-200 rounded-4xl hover:bg-black hover:text-white md:w-[140px]" style={{ padding: "12px" }}>
+    <div
+      className={`rounded-4xl ${
+        selected ? "bg-black text-white" : "bg-gray-200 text-black"
+      } transition duration-200 md:w-[140px]`}
+      style={{ padding: "12px" }}
+      onClick={onClick}
+    >
       <p className="text-center md:text-lg">{title}</p>
     </div>
   );

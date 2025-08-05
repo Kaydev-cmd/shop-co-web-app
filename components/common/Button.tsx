efe5a9900e1106388dfcbe54c68d24001f1657d2
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 15:17:00 +0200

    feat: add oprional 'type' prop to button component for flexibility

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ title, onClick, type = "button" }) => {
   return (
-    <button onClick={onClick} className="bg-black md:text-md">
+    <button
+      onClick={onClick}
+      type={type}
+      className="bg-black transition hover:bg-gray-800 md:text-md"
+    >
       {title}
     </button>
   );

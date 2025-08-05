e53d747e64176f7abc03a05f67b7346f4b9b72cc
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-05 08:59:00 +0200

    feat: add dynamic product search functionality with filter and results page

---
+++ pages/search/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import { PRODUCT_INFO } from "@/constants";
+import ProductCard from "@/components/common/ProductCard";
+import Link from "next/link";
+
+const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query")?.toLowerCase() || "";
+
+  const filteredProducts = PRODUCT_INFO.filter((product) => {
+    return (
+      product.productName.toLowerCase().includes(query) ||
+      product.productDescription.toLowerCase().includes(query)
+    );
+  });
+
+  return (
+    <section className="container">
+      <h1> Search Results for "{query}"</h1>
+
+      {filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-1">
+          {filteredProducts.map((product) => (
+            <Link href={`/product/${product.id}`} key={product.id}>
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                image={product.productImage}
+                alt={product.productName}
+                itemName={product.productName}
+                price={product.productPrice}
+                rating={product.productRating}
+                description={product.productDescription}
+              />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p>No products found matching your search.</p>
+      )}
+    </section>
+  );
+};
+
+export default SearchPage;

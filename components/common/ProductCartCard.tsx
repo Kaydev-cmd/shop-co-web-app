a238d242a00a67b82d599a575718f827ca86cfef
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 10:32:57 +0200

    feat: enable item removal from cart with removeFromCart handler

--- components/common/ProductCartCard.tsx
+++ components/common/ProductCartCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ProductCartCardProps } from "@/interfaces";
 import { FaTrashCan } from "react-icons/fa6";
-import Counter from "./Counter";
+import { useCart } from "@/context/CartContext";
 
 const ProductCartCard: React.FC<ProductCartCardProps> = ({
   productImage,
@@ -10,18 +10,38 @@ const ProductCartCard: React.FC<ProductCartCardProps> = ({
   productColor,
   productPrice,
 }) => {
+  const { removeFromCart } = useCart();
+
   return (
     <div
-      className="flex justify-around gap-4 items-center"
-      style={{ padding: "12px 0" }}
+      className="flex justify-around items-center"
+      style={{ padding: "4px 0" }}
     >
       <div>
-        <img src={productImage} alt={productName} className="rounded-xl" />
+        <img
+          src={productImage}
+          alt={productName}
+          className="rounded-xl w-[80%]"
+        />
       </div>
       <div className="flex flex-col gap-2">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-4">
           <h3 className="text-lg font-bold md:text-xl">{productName}</h3>
-          <FaTrashCan size={20} color="#FF3333" />
+          <FaTrashCan
+            size={20}
+            color="#FF3333"
+            onClick={() =>
+              removeFromCart({
+                id: productName,
+                productName,
+                productImage,
+                productSize,
+                productColor,
+                productPrice,
+                quantity: 1,
+              })
+            }
+          />
         </div>
         <p className="text-sm md:text-md">
           Size: <span className="text-[#666666]">{productSize}</span>{" "}
@@ -30,8 +50,7 @@ const ProductCartCard: React.FC<ProductCartCardProps> = ({
           Color: <span className="text-[#666666]">{productColor}</span>{" "}
         </p>
         <div className="flex items-center gap-12">
-          <p className="font-bold text-lg md:text-xl">{productPrice}</p>
-          <Counter />
+          <p className="font-bold text-lg md:text-xl">${productPrice}</p>
         </div>
       </div>
     </div>

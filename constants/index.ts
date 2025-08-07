eb834439085a9cb30ff093a58a14516ca54846e1
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 10:39:10 +0200

    feat: define COLORS array wit hex codes and names

--- constants/index.ts
+++ constants/index.ts
@@ -70,7 +70,7 @@ export const NEW_ARRIVALS = [
     image: "/assets/images/products/black_shirt.png",
     alt: "T-shirt with Tape Details",
     itemName: "T-shirt with Tape Details",
-    price: "$120",
+    price: 120,
     rating: "4.5",
   },
   {
@@ -78,7 +78,7 @@ export const NEW_ARRIVALS = [
     image: "/assets/images/products/jeans.png",
     alt: "Skinny Fit Jeans",
     itemName: "Skinny Fit Jeans",
-    price: "$240",
+    price: 240,
     rating: "4.5",
   },
   {
@@ -86,7 +86,7 @@ export const NEW_ARRIVALS = [
     image: "/assets/images/products/checkered_shirt.png",
     alt: "Checkered Shirt",
     itemName: "Checkered Shirt",
-    price: "$180",
+    price: 180,
     rating: "4.5",
   },
   {
@@ -94,7 +94,7 @@ export const NEW_ARRIVALS = [
     image: "/assets/images/products/sleeve_striped_shirt.png",
     alt: "Sleeve Striped T-Shirt",
     itemName: "Sleeve Striped T-Shirt",
-    price: "$130",
+    price: 130,
     rating: "4.5",
   },
   {
@@ -102,7 +102,7 @@ export const NEW_ARRIVALS = [
     image: "/assets/images/products/black_shirt.png",
     alt: "T-shirt with Tape Details",
     itemName: "T-shirt with Tape Details",
-    price: "$120",
+    price: 120,
     rating: "4.5",
   },
   {
@@ -110,7 +110,7 @@ export const NEW_ARRIVALS = [
     image: "/assets/images/products/jeans.png",
     alt: "Skinny Fit Jeans",
     itemName: "Skinny Fit Jeans",
-    price: "$240",
+    price: 240,
     rating: "4.5",
   },
 ];
@@ -121,7 +121,7 @@ export const TOP_SELLING = [
     image: "/assets/images/products/vertical_stripped.png",
     alt: "Vertical Stripped Shirt",
     itemName: "Vertical Stripped Shirt",
-    price: "$212",
+    price: 212,
     rating: "4.5",
   },
   {
@@ -129,7 +129,7 @@ export const TOP_SELLING = [
     image: "/assets/images/products/courage_graphic.png",
     alt: "Courage Graphic T-shirt",
     itemName: "Courage Graphic T-shirt",
-    price: "$145",
+    price: 145,
     rating: "4.5",
   },
   {
@@ -137,7 +137,7 @@ export const TOP_SELLING = [
     image: "/assets/images/products/loose_fit.png",
     alt: "Loose Fit Bermuda Short",
     itemName: "Loose Fit Bermuda Short",
-    price: "$80",
+    price: 80,
     rating: "4.5",
   },
   {
@@ -145,7 +145,7 @@ export const TOP_SELLING = [
     image: "/assets/images/products/faded_skinny.png",
     alt: "Faded Skinny Jeans",
     itemName: "Faded Skinny Jeans",
-    price: "$210",
+    price: 210,
     rating: "4.5",
   },
   {
@@ -153,7 +153,7 @@ export const TOP_SELLING = [
     image: "/assets/images/products/vertical_stripped.png",
     alt: "Vertical Stripped Shirt",
     itemName: "Vertical Stripped Shirt",
-    price: "$212",
+    price: 212,
     rating: "4.5",
   },
   {
@@ -161,7 +161,7 @@ export const TOP_SELLING = [
     image: "/assets/images/products/courage_graphic.png",
     alt: "Courage Graphic T-shirt",
     itemName: "Courage Graphic T-shirt",
-    price: "$145",
+    price: 145,
     rating: "4.5",
   },
 ];
@@ -294,7 +294,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/product_details/one_life.png",
     productName: "One Life Graphic T-Shirt",
     productRating: "4.5",
-    productPrice: "$260",
+    productPrice: 260,
     productDescription:
       "This graphic t-shirt which is perfect for any occasion. Crafted from a soft and breathable fabric, it offers superior comfort and style.",
   },
@@ -303,7 +303,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/black_shirt.png",
     productName: "T-shirt with Tape Details",
     productRating: "4.5",
-    productPrice: "$120",
+    productPrice: 120,
     productDescription:
       "This t-shirt with tape details is a perfect blend of style and comfort. Made from high-quality fabric, it features a modern design that adds a touch of sophistication to your wardrobe.",
   },
@@ -312,7 +312,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/jeans.png",
     productName: "Skinny Fit Jeans",
     productRating: "4.5",
-    productPrice: "$240",
+    productPrice: 240,
     productDescription:
       "These skinny fit jeans are a must-have for any fashion-forward individual. Made from durable denim, they offer a sleek and stylish look that pairs well with any outfit.",
   },
@@ -321,7 +321,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/checkered_shirt.png",
     productName: "Checkered Shirt",
     productRating: "4.5",
-    productPrice: "$180",
+    productPrice: 180,
     productDescription:
       "This checkered shirt is a timeless classic that never goes out of style. Made from soft cotton fabric, it provides comfort and versatility for any occasion.",
   },
@@ -330,7 +330,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/sleeve_striped_shirt.png",
     productName: "Sleeve Striped T-Shirt",
     productRating: "4.5",
-    productPrice: "$130",
+    productPrice: 130,
     productDescription:
       "This sleeve striped t-shirt is a perfect blend of style and comfort. Made from high-quality fabric, it features a modern design that adds a touch of sophistication to your wardrobe.",
   },
@@ -339,7 +339,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/vertical_stripped.png",
     productName: "Vertical Stripped Shirt",
     productRating: "4.5",
-    productPrice: "$212",
+    productPrice: 212,
     productDescription:
       "This vertical stripped shirt is a perfect blend of style and comfort. Made from high-quality fabric, it features a modern design that adds a touch of sophistication to your wardrobe.",
   },
@@ -348,7 +348,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/courage_graphic.png",
     productName: "Courage Graphic T-shirt",
     productRating: "4.5",
-    productPrice: "$145",
+    productPrice: 145,
     productDescription:
       "This courage graphic t-shirt is a perfect blend of style and comfort. Made from high-quality fabric, it features a modern design that adds a touch of sophistication to your wardrobe.",
   },
@@ -357,7 +357,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/loose_fit.png",
     productName: "Loose Fit Bermuda Short",
     productRating: "4.5",
-    productPrice: "$80",
+    productPrice: 80,
     productDescription:
       "These loose fit bermuda shorts are a perfect blend of style and comfort. Made from high-quality fabric, they feature a modern design that adds a touch of sophistication to your wardrobe.",
   },
@@ -366,7 +366,7 @@ export const PRODUCT_INFO = [
     productImage: "/assets/images/products/faded_skinny.png",
     productName: "Faded Skinny Jeans",
     productRating: "4.5",
-    productPrice: "$210",
+    productPrice: 210,
     productDescription:
       "These faded skinny jeans are a must-have for any fashion-forward individual. Made from durable denim, they offer a sleek and stylish look that pairs well with any outfit.",
   },
@@ -379,7 +379,7 @@ export const CART_ITEMS = [
     productName: "Vertical Stripped",
     productSize: "Large",
     productColor: "Green",
-    productPrice: "$212",
+    productPrice: 212,
   },
   {
     id: "courage-graphic",
@@ -387,7 +387,7 @@ export const CART_ITEMS = [
     productName: "Courage Graphic",
     productSize: "Medium",
     productColor: "Orange",
-    productPrice: "$180",
+    productPrice: 180,
   },
   {
     id: "loose-fit",
@@ -395,6 +395,21 @@ export const CART_ITEMS = [
     productName: "Loose Fit",
     productSize: "Large",
     productColor: "Blue",
-    productPrice: "$240",
+    productPrice: 240,
+  },
+];
+
+export const COLORS = [
+  {
+    hex: "#4F4631",
+    name: "Brown",
+  },
+  {
+    hex: "#314F4A",
+    name: "Teal",
+  },
+  {
+    hex: "#31344F",
+    name: "Navy",
   },
 ];

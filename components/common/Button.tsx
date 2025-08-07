ffd8c8e2d8568bc421c73dbd61b9461f0163f1b1
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 15:06:42 +0200

    style: align button to center

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -3,13 +3,15 @@ import { ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({ title, onClick, type = "button" }) => {
   return (
-    <button
-      onClick={onClick}
-      type={type}
-      className="bg-black transition hover:bg-gray-800 md:text-md"
-    >
-      {title}
-    </button>
+    <div className="flex justify-center">
+      <button
+        onClick={onClick}
+        type={type}
+        className="bg-black transition hover:bg-gray-800 md:text-md"
+      >
+        {title}
+      </button>
+    </div>
   );
 };
 

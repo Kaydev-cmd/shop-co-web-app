c146fc052e77a57e99e8bdda9e9345998682d09e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 11:51:01 +0200

    feat: add scroll-bassed animation with framer-motion to improve section entrance effects

--- components/common/Hero.tsx
+++ components/common/Hero.tsx
@@ -3,12 +3,18 @@ import Button from "./Button";
 import { HERO_LOGOS } from "@/constants";
 import HeroStats from "./HeroStats";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const router = useRouter();
 
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.8 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
       <div className="container">
         <div className="flex flex-col gap-6">
           <h2 className="font-extrabold text-4xl uppercase md:text-center md:text-6xl">
@@ -52,7 +58,7 @@ const Hero = () => {
           />
         ))}
       </div>
-    </>
+    </motion.div>
   );
 };
 

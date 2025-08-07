e1cc6468ca84f1290da1b8a97445e0f022e38152
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 10:29:54 +0200

    feat: add increment and decrement functionality for item quantity

--- components/common/Counter.tsx
+++ components/common/Counter.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa";
+import { CounterProps } from "@/interfaces";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const Counter: React.FC<CounterProps> = ({ value, onChange }) => {
+  const increment = () => onChange(value + 1);
+  const decrement = () => {
+    if (value > 1) onChange(value - 1);
+  };
 
   return (
     <div
       className="bg-gray-200 rounded-4xl flex items-center justify-center gap-6 md:w-[160px] md:text-md"
       style={{ padding: "12px 16px" }}
     >
-      <FaMinus onClick={() => setCount((prev) => (prev > 0 ? prev - 1 : 0))} />
-      <p>{count}</p>
-      <FaPlus onClick={() => setCount((prev) => prev + 1)} />
+      <FaMinus onClick={decrement} />
+      <p>{value}</p>
+      <FaPlus onClick={increment} />
     </div>
   );
 };

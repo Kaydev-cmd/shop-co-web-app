e6e26071dd1e9e88995c0c26aaa4d04cf6b633f3
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 11:50:46 +0200

    feat: add scroll-bassed animation with framer-motion to improve section entrance effects

--- components/common/Banner.tsx
+++ components/common/Banner.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
-    <div className="bg-black text-white flex items-center" style={{ padding: "6px 12px" }}>
-      <p className="container flex flex-col text-center">
-        Sign up and get 20% off of your first order.{" "}
-        <a href="#" className="underline">
-          Sign Up Now
-        </a>
-      </p>
-      <IoClose size={28}/>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.8 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <div
+        className="bg-black text-white flex items-center"
+        style={{ padding: "6px 12px" }}
+      >
+        <p className="container flex flex-col text-center">
+          Sign up and get 20% off of your first order.{" "}
+          <a href="#" className="underline">
+            Sign Up Now
+          </a>
+        </p>
+        <IoClose size={28} />
+      </div>
+    </motion.div>
   );
 };
 

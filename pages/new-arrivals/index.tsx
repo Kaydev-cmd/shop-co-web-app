939294795a5a16d3f5e31ed5fb4ac875b428f395
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 11:52:02 +0200

    feat: add scroll-bassed animation with framer-motion to improve section entrance effects

--- pages/new-arrivals/index.tsx
+++ pages/new-arrivals/index.tsx
@@ -4,34 +4,51 @@ import Link from "next/link";
 import ProductCard from "@/components/common/ProductCard";
 import Button from "@/components/common/Button";
 import Subscribe from "@/components/common/Subscribe";
+import { motion } from "framer-motion";
 
 const NewArrivalsPage = () => {
   return (
     <>
-      <section>
-        <div className="container flex flex-col gap-4">
-          <h3 className="font-extrabold text-4xl text-center">New Arrivals</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {NEW_ARRIVALS.map((product) => (
-              <Link href={`product/${product.id}`} key={product.id}>
-                <ProductCard
-                  key={product.itemName}
-                  image={product.image}
-                  alt={product.alt}
-                  rating={product.rating}
-                  itemName={product.itemName}
-                  price={product.price}
-                />
-              </Link>
-            ))}
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        transition={{ duration: 0.8 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+      >
+        <section>
+          <div className="container flex flex-col gap-4">
+            <h3 className="font-extrabold text-4xl text-center">
+              New Arrivals
+            </h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              {NEW_ARRIVALS.map((product) => (
+                <Link href={`product/${product.id}`} key={product.id}>
+                  <ProductCard
+                    key={product.itemName}
+                    image={product.image}
+                    alt={product.alt}
+                    rating={product.rating}
+                    itemName={product.itemName}
+                    price={product.price}
+                  />
+                </Link>
+              ))}
+            </div>
+            <Button title="View More" />
           </div>
-          <Button title="View More" />
-        </div>
-      </section>
+        </section>
+      </motion.div>
 
-      <section className="container">
-        <Subscribe />
-      </section>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        transition={{ duration: 0.8 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+      >
+        <section className="container">
+          <Subscribe />
+        </section>
+      </motion.div>
     </>
   );
 };

0ff57788bbf6cbbf97f8891aba23119b30994398
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 11:51:29 +0200

    feat: add scroll-bassed animation with framer-motion to improve section entrance effects

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,6 +7,7 @@ import { IoMdSearch } from "react-icons/io";
 import { useRouter } from "next/router";
 import SearchBar from "../common/SearchBar";
 import CartIcon from "../common/CartIcon";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Header: React.FC = () => {
   const router = useRouter();
@@ -31,59 +32,74 @@ const Header: React.FC = () => {
   const handleNav = () => setNav(!nav);
 
   return (
-    <header>
-      <div className="container flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <CgMenu size={32} onClick={handleNav} className="md:hidden" />
-          {!searchOpen && (
-            <h1
-              className="uppercase font-extrabold text-2xl"
-              onClick={() => router.push("/")}
-            >
-              Shop.co
-            </h1>
-          )}
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.8 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <header>
+        <div className="container flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <div ref={navRef}>
+              <CgMenu size={32} onClick={handleNav} className="md:hidden" />
+            </div>
+            {!searchOpen && (
+              <h1
+                className="uppercase font-extrabold text-2xl"
+                onClick={() => router.push("/")}
+              >
+                Shop.co
+              </h1>
+            )}
+          </div>
+          {searchOpen && <SearchBar />}
+
+          {/* Desktop Links */}
+          <nav className="hidden md:flex items-center gap-6">
+            {NAV_LINKS.map((link) => (
+              <Link key={link.id} href={link.href}>
+                {link.link}
+              </Link>
+            ))}
+          </nav>
+
+          {/* Mobile Links */}
+          <AnimatePresence>
+            {nav && (
+              <motion.nav
+                ref={navRef}
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="flex flex-col items-center justify-center gap-6 absolute top-32 left-0 w-full bg-black text-white h-72 z-50"
+              >
+                {NAV_LINKS.map((link) => (
+                  <Link
+                    key={link.id}
+                    href={link.href}
+                    className="text-lg"
+                    onClick={() => setNav(false)}
+                  >
+                    {link.link}
+                  </Link>
+                ))}
+              </motion.nav>
+            )}
+          </AnimatePresence>
+
+          <div className="flex items-center gap-4">
+            <IoMdSearch
+              size={32}
+              onClick={() => setSearchOpen((prev) => !prev)}
+            />
+            <CartIcon />
+            <FaRegUserCircle size={28} onClick={() => router.push("/user")} />
+          </div>
         </div>
-        {searchOpen && <SearchBar />}
-
-        {/* Desktop Links */}
-        <nav className="hidden md:flex items-center gap-6">
-          {NAV_LINKS.map((link) => (
-            <Link key={link.id} href={link.href}>
-              {link.link}
-            </Link>
-          ))}
-        </nav>
-
-        {/* Mobile Links */}
-        <nav
-          ref={navRef}
-          className={`${
-            nav ? "flex" : "hidden"
-          } flex-col items-center justify-center gap-6 absolute top-34 left-0 w-full bg-black text-white transition-all duration-300 ease-in-out h-72`}
-        >
-          {NAV_LINKS.map((link) => (
-            <Link
-              key={link.id}
-              href={link.href}
-              className="text-lg"
-              onClick={() => setNav(false)}
-            >
-              {link.link}
-            </Link>
-          ))}
-        </nav>
-
-        <div className="flex items-center gap-4">
-          <IoMdSearch
-            size={32}
-            onClick={() => setSearchOpen((prev) => !prev)}
-          />
-          <CartIcon />
-          <FaRegUserCircle size={28} onClick={() => router.push("/user")} />
-        </div>
-      </div>
-    </header>
+      </header>
+    </motion.div>
   );
 };
 

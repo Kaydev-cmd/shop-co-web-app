572bcb8f7c49069151f7cccc3e4b1bb339d5cd8e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 11:51:39 +0200

    feat: add scroll-bassed animation with framer-motion to improve section entrance effects

--- components/products/ProductDetails.tsx
+++ components/products/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductInfoCard from "../common/ProductInfoCard";
 import { ProductInfoCardProps } from "@/interfaces";
+import { motion } from "framer-motion";
 
 interface ProductDetailsProps {
   product: ProductInfoCardProps;
@@ -10,16 +11,23 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
   if (!product) return null;
 
   return (
-    <div className="container flex flex-col gap-3">
-      <ProductInfoCard
-        key={product.productName}
-        productImage={product.productImage}
-        productName={product.productName}
-        productRating={product.productRating}
-        productPrice={product.productPrice}
-        productDescription={product.productDescription}
-      />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.8 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <div className="container flex flex-col gap-3">
+        <ProductInfoCard
+          key={product.productName}
+          productImage={product.productImage}
+          productName={product.productName}
+          productRating={product.productRating}
+          productPrice={product.productPrice}
+          productDescription={product.productDescription}
+        />
+      </div>
+    </motion.div>
   );
 };
 

8b92ff9c1f2a3c1217f10e739c4c12bd0ca7e6df
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-07 10:28:58 +0200

    feat: add dynamic cart icon with item count badge and navigation

---
+++ components/common/CartIcon.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useCart } from "@/context/CartContext";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { useRouter } from "next/router";
+
+const CartIcon = () => {
+  const { cartItems } = useCart();
+  const router = useRouter();
+
+  const totalItems = cartItems.reduce(
+    (acc, cartItem) => acc + cartItem.quantity,
+    0
+  );
+
+  return (
+    <div className="relative">
+      <MdOutlineShoppingCart size={28} onClick={() => router.push("/cart")} />
+      {totalItems > 0 && (
+        <span
+          className="absolute bottom-4 left-4 bg-red-500 text-white text-xs rounded-full"
+          style={{ padding: "0px 4px" }}
+        >
+          {totalItems}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default CartIcon;

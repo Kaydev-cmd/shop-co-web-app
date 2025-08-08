12e5d5d7a442aea03ff8a774a25db64add5e10cf
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-08 08:17:31 +0200

    refactor: remove unused useState import

--- components/common/Pill.tsx
+++ components/common/Pill.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title, selected, onClick }) => {
